c94d24fa20e9d101f33ffac297a0920946f6bd40
SalsaGal <fielddashos@gmail.com>
2023-11-26 16:50:11 +1100

    Add new bear button

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use bear::Bear;
 use eframe::{App, NativeOptions};
+use rand::rngs::ThreadRng;
 
 mod bear;
 
@@ -9,18 +11,31 @@ fn main() {
     eframe::run_native(
         "Honey Heist",
         options,
-        Box::new(|cc| Box::new(HoneyApp::default())),
+        Box::new(|_| Box::<HoneyApp>::default()),
     )
     .unwrap();
 }
 
 #[derive(Default)]
-struct HoneyApp {}
+struct HoneyApp {
+    rng: ThreadRng,
+    bear: Option<Bear>,
+}
 
 impl App for HoneyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::top("control").show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                ui.heading("Control");
+                if ui.button("New Bear").clicked() {
+                    self.bear = Some(Bear::new(&mut self.rng, "Foo".to_owned()));
+                }
+            });
+        });
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Honey Heist");
+            if let Some(bear) = &self.bear {
+                ui.heading(&bear.name);
+            }
         });
     }
 }
