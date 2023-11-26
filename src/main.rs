da305ed01fa5d57b93b25df45ec9ce4b70d2e5ac
SalsaGal <fielddashos@gmail.com>
2023-11-26 16:41:45 +1100

    Add eframe

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use rand::thread_rng;
-
-use crate::bear::Bear;
+use eframe::{App, NativeOptions};
 
 mod bear;
 
 fn main() {
-    let mut rng = thread_rng();
-    let bear = Bear::new(&mut rng, "Beepboop".to_owned());
-    println!("{}", toml::to_string_pretty(&bear).unwrap());
+    let options = NativeOptions {
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Honey Heist",
+        options,
+        Box::new(|cc| Box::new(HoneyApp::default())),
+    )
+    .unwrap();
+}
+
+#[derive(Default)]
+struct HoneyApp {}
+
+impl App for HoneyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Honey Heist");
+        });
+    }
 }
