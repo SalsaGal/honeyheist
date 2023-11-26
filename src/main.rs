352a130237c0cab6522221ce0348648cb61c1595
SalsaGal <fielddashos@gmail.com>
2023-11-26 18:09:21 +1100

    Add loading and saving

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::fs::{read_to_string, File};
+use std::io::Write;
+
 use bear::Bear;
 use eframe::{App, NativeOptions};
 use rand::rngs::ThreadRng;
+use rfd::FileDialog;
 
 mod bear;
 
@@ -29,6 +33,25 @@ impl App for HoneyApp {
                 ui.heading("Honey Heist");
                 if ui.button("New Bear").clicked() {
                     self.bear = Some(Bear::new(&mut self.rng, "Foo".to_owned()));
+                }
+                if ui.button("Load Bear").clicked() {
+                    self.bear = FileDialog::new()
+                        .add_filter("toml", &["toml"])
+                        .pick_file()
+                        .and_then(|path| read_to_string(path).ok())
+                        .and_then(|contents| toml::from_str(&contents).ok());
+                }
+                if let Some(bear) = &self.bear {
+                    if ui.button("Save Bear").clicked() {
+                        if let Some(mut file) = FileDialog::new()
+                            .add_filter("toml", &["toml"])
+                            .save_file()
+                            .map(|path| path.with_extension("toml"))
+                            .and_then(|path| File::create(path).ok())
+                        {
+                            write!(file, "{}", toml::to_string(bear).unwrap()).unwrap();
+                        }
+                    }
                 }
             });
         });
