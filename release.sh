6060386044b27a9cf543609cf0001b1b1cdededa
SalsaGal <fielddashos@gmail.com>
2023-12-03 15:50:51 +1100

    Use release bash script

---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+version_line=$(grep "^version" Cargo.toml)
+readarray -d "\"" -t version <<< "$version_line"
+
+cargo build --release
+RUSTFLAGS='-L /usr/x86_64-w64-mingw32/lib' cargo zigbuild --target x86_64-pc-windows-gnu --release
+
+cd target/release
+zip -r "../linux-${version[1]}.zip" honeyheist
+
+cd ../x86_64-pc-windows-gnu/release
+zip -r "../../windows${version[1]}.zip" honeyheist.exe
